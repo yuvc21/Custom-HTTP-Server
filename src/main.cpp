e860703e80ec38bec5cb130a4fa57f21bad8839c
yuvc21 <panduraj06211@gmail.com>
2025-12-17 21:59:47 +0530

    sending 200 response to the client

--- src/main.cpp
+++ src/main.cpp
@@ -52,11 +52,9 @@ int main(int argc, char **argv) {
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
 
-  std::cout << "Waiting for a client to connect...\n";
-
   // it blocks the code up till here until a client connects.
   std::cout << "Waiting for a client to connect..." << std::endl;
-  int client_fd - accept(server_fd, (struct sockaddr *)&client_addr,
+  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
                          (socklen_t *)&client_addr_len);
   std::cout << "Client Connected" << std::endl;
 
